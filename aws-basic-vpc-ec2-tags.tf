0a585d5080ff112d571eecc52218c3727d3d0b70
NoelXP <npres29@wgu.edu>
2024-05-11 02:22:18 +0000

    Added terraform .gitignore to ignore state. Added VPC and public subnet.

--- aws-basic-vpc-ec2-tags.tf
+++ aws-basic-vpc-ec2-tags.tf
@@ -5,7 +5,7 @@ terraform {
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            version = "-> 5.0"
+            version = "~> 5.0"
         }
     }
 }
@@ -15,4 +15,22 @@ provider "aws" {
     shared_credentials_files = ["~/.aws/credentials"]
     profile                  = "default"
     region                   = "us-east-2"
+}
+
+# VPC
+resource "aws_vpc" "taa-vpc" {
+    cidr_block = 172.21.0.0/19 #172.21.0.0 - 172.21.31.254
+    tags  = {
+        Name = "taa-vpc"
+    }
+}
+
+# Subnet
+resource "aws_subnet" "taa-public-sub-00" {
+    vpc_id                  = aws_vpc.taa-vpc.id
+    cidr_block              = "172.21.0.0/23" #172.21.0.0 - 172.21.1.255
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "taa-public-sub-00"
+    }
 }

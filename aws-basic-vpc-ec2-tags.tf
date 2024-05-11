d31e168a2622f7315af21979dc20514eb8bb22fa
ec2-user <npres29@wgu.edu>
2024-05-11 02:03:23 +0000

    Defined the required Terraform version and AWS required provider

---
+++ aws-basic-vpc-ec2-tags.tf
@@ -0,0 +1,18 @@
+#Define Terraform Providers and Backend
+terraform {
+    required_version = "> 1.5"
+    
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "-> 5.0"
+        }
+    }
+}
+
+#Default provider: AWS
+provider "aws" {
+    shared_credentials_files = ["~/.aws/credentials"]
+    profile                  = "default"
+    region                   = "us-east-2"
+}
